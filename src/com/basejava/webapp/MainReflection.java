462ff0c0f8736e44ff2bfdf72bba2265f1cf49e9
unknown <ulmoniss@yandex.ru>
2021-02-14 14:55:32 +0300

    Reflection attempt2

--- src/com/basejava/webapp/MainReflection.java
+++ src/com/basejava/webapp/MainReflection.java
@@ -2,6 +2,7 @@ package com.basejava.webapp;
 
 import com.basejava.webapp.model.Resume;
 import com.basejava.webapp.storage.AbstractArrayStorage;
+import com.basejava.webapp.storage.SortedArrayStorage;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -17,14 +18,18 @@ public class MainReflection {
         field.set(r, "new_uuid");
 
         // TODO : invoke r.toString via reflection
-        System.out.println(Resume.class.getMethod("toString", null).invoke(r, null));
+        System.out.println(Resume.class.getMethod("toString").invoke(r));
 
         //IllegalAccessException: class com.basejava.webapp.MainReflection cannot access a member of class com.basejava.webapp.storage.AbstractArrayStorage with modifiers "protected static final"
         Field f = AbstractArrayStorage.class.getDeclaredField("STORAGE_LIMIT");
         f.setAccessible(true);
-        System.out.println(AbstractArrayStorage.class.getDeclaredField("STORAGE_LIMIT").get(null));
+        Object value = f.get(AbstractArrayStorage.class);
+        System.out.println(AbstractArrayStorage.class.getDeclaredField("STORAGE_LIMIT").get(new SortedArrayStorage()));
 
-
-
+        //Чтобы получить значение поля, нужно сначала получить для этого поля объект типа Field затем
+        // использовать метод get(). Метод принимает входным параметром ссылку на объект класса.
+        //Class c = obj.getClass();
+        //Field field = c.getField("name");
+        //String nameValue = (String) field.get(obj)
     }
 }
