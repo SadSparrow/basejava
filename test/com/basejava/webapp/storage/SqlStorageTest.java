50ef7b735c46746b239f6c977b4442f709479524
unknown <ulmoniss@yandex.ru>
2021-05-15 11:50:33 +0300

    sqlStorageTest

--- test/com/basejava/webapp/storage/SqlStorageTest.java
+++ test/com/basejava/webapp/storage/SqlStorageTest.java
@@ -3,9 +3,8 @@ package com.basejava.webapp.storage;
 import com.basejava.webapp.Config;
 
 public class SqlStorageTest extends AbstractStorageTest{
-    private static final SqlStorage SQL_STORAGE = Config.get().getStorage();
 
     public SqlStorageTest() {
-        super(SQL_STORAGE);
+        super(Config.get().getStorage());
     }
 }
